3c636e8fae07be03d17cbe75dff87989459fbdfe
suttipong klinhom <suttipkl@ais.co.th>
2025-09-05 15:46:16 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,22 @@
 const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  // publicPath: process.env.NODE_ENV === 'production' ? '/downloader/' : '/',
-  publicPath: '/downloader/',
+  publicPath: '/downloader/', // path สำหรับ production
+
   devServer: {
-    allowedHosts: "all", // ปิดการเช็ค host header
-    host: "0.0.0.0",     // ให้ container อื่นเข้าถึงได้
-    port: 8080           // หรือพอร์ตที่คุณใช้
+    allowedHosts: "all",   // ปิดการเช็ค host header
+    host: "0.0.0.0",       // ให้ container หรือ network อื่นเข้าถึงได้
+    port: 8080,            // พอร์ต dev server
+
+    // กำหนด WebSocket สำหรับ HMR ให้เข้าถึงจาก domain จริง
+    client: {
+      webSocketURL: {
+        hostname: "gomo.mimotech.org", // ชื่อ domain ที่ browser ใช้
+        port: 8080,
+        protocol: "ws"
+      }
+    }
   }
 });
