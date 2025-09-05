e3250e3782aca88f60994814720a9cf0a0018f71
suttipong klinhom <suttipkl@ais.co.th>
2025-09-05 15:28:01 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/downloader/' : '/',
+  // publicPath: process.env.NODE_ENV === 'production' ? '/downloader/' : '/',
+  publicPath: '/downloader/',
   devServer: {
     allowedHosts: "all", // ปิดการเช็ค host header
     host: "0.0.0.0",     // ให้ container อื่นเข้าถึงได้
