0cac9f8f847e589c0b627eb2da1b81fc4f3cd3c8
suttipong klinhom <suttipkl@ais.co.th>
2025-09-05 15:21:28 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,10 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/gomo-downloader/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/gomo-downloader/' : '/',
+  devServer: {
+    allowedHosts: "all", // ปิดการเช็ค host header
+    host: "0.0.0.0",     // ให้ container อื่นเข้าถึงได้
+    port: 8080           // หรือพอร์ตที่คุณใช้
+  }
 });
